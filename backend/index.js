1ebb1262218f48fa6770f1b1f07e321d74a30ae4
vishal-git9 <vishalsinghrajawat990@gmail.com>
2023-06-10 23:33:18 +0530

    basic frontend and backend setup is done

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+require("dotenv").config()
+const app = express()
+
+
+app.listen(`${process.env.PORT}`,async()=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+
+    console.log(`connected to port ${process.env.PORT}`)
+})
