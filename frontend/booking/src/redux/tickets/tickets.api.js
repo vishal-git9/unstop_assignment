d692f0cfd7acd790def9182d911c4d3f8529c5b5
vishal-git9 <vishalsinghrajawat990@gmail.com>
2023-06-11 21:55:09 +0530

    deployed backend url is integrated

--- frontend/booking/src/redux/tickets/tickets.api.js
+++ frontend/booking/src/redux/tickets/tickets.api.js
@@ -1,19 +1,19 @@
 import axios from "axios"
 export const getTicketSeatsData = async()=>{
 
-    const res = await axios("http://localhost:4500/api/")
+    const res = await axios("https://fastbook-352t.onrender.com/api/")
     const {coachData} = res.data
     return coachData
 
 }
 export const bookTicketSeatsData = async(data)=>{
-    const res = await axios.patch("http://localhost:4500/api/bookSeats",data)
+    const res = await axios.patch("https://fastbook-352t.onrender.com/api/bookSeats",data)
     const {msg} = res.data
     return msg
 }
 
 export const getUnbookedData = async()=>{
-    const res = await axios.get("http://localhost:4500/api/unBooked")
+    const res = await axios.get("https://fastbook-352t.onrender.com/api/unBooked")
     const {coachData} = res.data
     return coachData
 }
